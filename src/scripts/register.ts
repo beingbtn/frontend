0dae69c5057d781304f46f09b4e4b3b11654363a
Attituding <84680760+Attituding@users.noreply.github.com>
2022-09-17 17:05:23 -0700

    feat: finish register page

--- src/scripts/register.ts
+++ src/scripts/register.ts
@@ -8,11 +8,14 @@ const output = document.getElementById('output') as HTMLSpanElement;
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    console.log(username.value, password.value);
-
     try {
         await fetch(`https://btn.attituding.workers.dev/register?username=${username.value}&password=${password.value}`);
     } catch (error) {
         output.textContent = (error as Error)?.message;
     }
+
+    username.value = '';
+    password.value = '';
+
+    // window.location.href = '/home';
 });
