128a383c7d35ee81ac73ff2caf4f5fcf75ce422c
Attituding <84680760+Attituding@users.noreply.github.com>
2022-09-17 17:14:02 -0700

    feat: login page

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -1,1 +1,23 @@
 export {};
+
+const form = document.getElementById('form') as HTMLFormElement;
+const username = document.getElementById('username') as HTMLInputElement;
+const password = document.getElementById('password') as HTMLInputElement;
+const output = document.getElementById('output') as HTMLSpanElement;
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    try {
+        await fetch(
+            `https://btn.attituding.workers.dev/login?username=${username.value}&password=${password.value}`,
+        );
+
+        username.value = '';
+        password.value = '';
+
+        window.location.href = '/home';
+    } catch (error) {
+        output.textContent = (error as Error)?.message;
+    }
+});
--- src/scripts/register.ts
+++ src/scripts/register.ts
@@ -9,13 +9,15 @@ form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     try {
-        await fetch(`https://btn.attituding.workers.dev/register?username=${username.value}&password=${password.value}`);
+        await fetch(
+            `https://btn.attituding.workers.dev/register?username=${username.value}&password=${password.value}`,
+        );
+
+        username.value = '';
+        password.value = '';
+
+        window.location.href = '/home';
     } catch (error) {
         output.textContent = (error as Error)?.message;
     }
-
-    username.value = '';
-    password.value = '';
-
-    // window.location.href = '/home';
 });
