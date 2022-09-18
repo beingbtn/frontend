72c3a1bd3d824febe784c7f41189f97f8b620bc4
Attituding <84680760+Attituding@users.noreply.github.com>
2022-09-18 13:39:09 -0700

    feat: register page

--- src/scripts/register.ts
+++ src/scripts/register.ts
@@ -1,40 +1,8 @@
-export {};
+const scopes = ['identity'].join('%20');
+const clientId = '1021144846909579346';
+const state = window.crypto.randomUUID();
+const url = `https://discord.com/oauth2/authorize?response_type=token&client_id=${clientId}&state=${state}&scope=${scopes}`;
 
-const form = document.getElementById('form') as HTMLFormElement;
-const username = document.getElementById('username') as HTMLInputElement;
-const password = document.getElementById('password') as HTMLInputElement;
-const output = document.getElementById('output') as HTMLSpanElement;
+localStorage.setItem('state', window.crypto.randomUUID());
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    try {
-        output.textContent = '';
-
-        const response = await fetch(
-            `https://btn.attituding.workers.dev/register?username=${username.value}&password=${password.value}`,
-        );
-
-        const text = await response.text();
-
-        console.log(text, response.statusText);
-
-        switch (response.status) {
-            case 200:
-                username.value = '';
-                password.value = '';
-
-                window.location.href = '/home';
-                break;
-            case 403:
-                username.value = '';
-                password.value = '';
-
-                output.textContent = text || response.statusText;
-            default: 
-                output.textContent = text || response.statusText;
-        }
-    } catch (error) {
-        output.textContent = (error as Error)?.message;
-    }
-});
+window.location.replace(url);
