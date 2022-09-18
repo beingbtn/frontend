ad605f61a1410d555b000dd3b363e9675c2c8f17
Attituding <84680760+Attituding@users.noreply.github.com>
2022-09-18 14:19:23 -0700

    fjnasfijfasaf

--- src/scripts/register.ts
+++ src/scripts/register.ts
@@ -8,12 +8,18 @@ const expiresInFragment = fragment.get('expires_in');
 const scopeFragment = fragment.get('scope');
 const stateFragment = fragment.get('state');
 
-if (accessTokenFragment && tokenTypeFragment && expiresInFragment && scopeFragment && stateFragment) {
+if (
+    accessTokenFragment &&
+    tokenTypeFragment &&
+    expiresInFragment &&
+    scopeFragment &&
+    stateFragment
+) {
     if (stateFragment === localStorage.getItem('state')) {
-        const url = `https://btn.attituding.workers.dev/register?access_token=${accessTokenFragment}`;
+        const url = `https://btn.attituding.workers.dev/register?access_token=${accessTokenFragment}&token_type=${tokenTypeFragment}`;
 
         try {
-            await fetch(url);
+            await fetch(url, { method: 'POST' });
 
             window.location.replace('/members');
         } catch (error) {
@@ -27,7 +33,7 @@ if (accessTokenFragment && tokenTypeFragment && expiresInFragment && scopeFragme
     const clientId = '1021144846909579346';
     const state = window.crypto.randomUUID();
     const url = `https://discord.com/oauth2/authorize?response_type=token&client_id=${clientId}&state=${state}&scope=${scopes}`;
-    
+
     localStorage.setItem('state', window.crypto.randomUUID());
 
     window.location.replace(url);
